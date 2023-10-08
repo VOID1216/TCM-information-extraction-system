0ae61394381b494f2ee0b8ac3e6a9ec1649418cb
VOID1216 <82436830+VOID1216@users.noreply.github.com>
2023-10-08 12:00:53 +0800

    Delete action directory

--- action/deleteArticle.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../db_config.php';
-if (isset($_GET)){
-    $articleId = $_GET['id'];
-    $queryString="delete from assignment  where id='$articleId'";
-    mysqli_query($link,$queryString);
-    echo mysqli_error($link);
-    echo 'success';
-}
